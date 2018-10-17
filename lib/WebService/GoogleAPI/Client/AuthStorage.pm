866847427017fcae59e665d50b2b64c152bfd5f1
Peter Scott <peter@computerpros.com.au>
2018-10-17 22:30:00 +1000

    First source commit
    I was developing this privately in the WebService::Google::Client namespace but gave up trying to collaborate with the holder of the cpan package space. Retaining it privately seemed like a waste and was in some cases problematic so I settled on the new namespace and pushed to cpan. I will clean up over the coming days and continue to work on refining.

---
+++ lib/WebService/GoogleAPI/Client/AuthStorage.pm
@@ -0,0 +1,87 @@
+use strictures; 
+package WebService::GoogleAPI::Client::AuthStorage;
+
+# ABSTRACT: JSON File Persistence for Google OAUTH Project and User Access Tokens
+
+## is client->auth_storage
+## or is Client->ua->auth_storage delegated as auth_storage to client 
+
+## or is UserAgent->credentials
+
+use 5.014;
+use Moo;
+use Carp;
+use WebService::GoogleAPI::Client::AuthStorage::ConfigJSON;
+
+
+has 'storage' => ( is => 'rw', default => sub { WebService::GoogleAPI::Client::AuthStorage::ConfigJSON->new } );    # by default
+has 'is_set'  => ( is => 'rw', default => 0 );
+
+
+
+=method setup
+
+Set appropriate storage
+
+  my $auth_storage = WebService::GoogleAPI::Client::AuthStorage->new;
+  $auth_storage->setup; # by default will be config.json
+  $auth_storage->setup({type => 'jsonfile', path => '/abs_path' });
+
+
+=cut
+
+sub setup
+{
+  my ( $self, $params ) = @_;
+  if ( $params->{ type } eq 'jsonfile' )
+  {
+    $self->storage->pathToTokensFile( $params->{ path } );
+    $self->storage->setup;
+    $self->is_set( 1 );
+  }
+  else
+  {
+    croak "Unknown storage type. Allowed types are jsonfile, dbi and mongo";
+  }
+  return $self;
+}
+
+### Below are list of methods that each Storage subclass must provide
+
+=method get_credentials_for_refresh
+
+Return all parameters that is needed for Mojo::Google::AutoTokenRefresh::refresh_access_token() function: client_id, client_secret and refresh_token
+
+$c->get_credentials_for_refresh('examplemail@gmail.com')
+
+This method must have all subclasses of WebService::GoogleAPI::Client::AuthStorage
+
+=cut
+
+sub get_credentials_for_refresh
+{
+  my ( $self, $user ) = @_;
+  return $self->storage->get_credentials_for_refresh( $user );
+}
+
+sub get_access_token_from_storage
+{
+  my ( $self, $user ) = @_;
+  return $self->storage->get_access_token_from_storage( $user );
+}
+
+sub set_access_token_to_storage
+{
+  my ( $self, $user, $access_token ) = @_;
+  return $self->storage->set_access_token_to_storage( $user, $access_token );
+}
+
+
+sub get_scopes_from_storage_as_array
+{
+  my ( $self ) = @_;
+  return $self->storage->get_scopes_from_storage_as_array();
+}
+
+
+1;
---
+++ lib/WebService/GoogleAPI/Client/Credentials.pm
@@ -0,0 +1,51 @@
+use strictures; 
+package WebService::GoogleAPI::Client::Credentials;
+
+# ABSTRACT: Credentials for particular Client instance. You can use this module as singleton also if you need to share
+#           credentials between two or more modules
+
+
+use Carp;
+use Moo;
+with 'MooX::Singleton';
+
+
+has 'access_token' => ( is => 'rw' );
+has 'user'         => ( is => 'rw', trigger => \&get_access_token_for_user );                             # full gmail, like peter@shotgundriver.com
+has 'auth_storage' => ( is => 'rw', default => sub { WebService::GoogleAPI::Client::AuthStorage->new } );    # dont delete to able to configure
+
+=method get_access_token_for_user
+
+Automatically get access_token for current user if auth_storage is set
+
+=cut
+
+sub get_access_token_for_user
+{
+  my ( $self ) = @_;
+  if ( $self->auth_storage->is_set )
+  {                                                                                                       # chech that auth_storage initialized fine
+    $self->access_token( $self->auth_storage->get_access_token_from_storage( $self->user ) );
+  }
+  else
+  {
+    croak q/Can get access token for specified user because storage isn't set/;
+  }
+  return $self; ## ?? is self the access token for user?
+}
+
+sub get_scopes_as_array
+{
+  my ( $self ) = @_;
+  if ( $self->auth_storage->is_set )
+  {                                                                                                       # chech that auth_storage initialized fine
+    return $self->access_token( $self->auth_storage->get_scopes_from_storage_as_array() );
+  }
+  else
+  {
+    croak q/Can get access token for specified user because storage isn't set/;
+  }
+  
+}
+
+1;
